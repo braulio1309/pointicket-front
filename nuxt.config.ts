5b491d675bec1a3eb01a4ad58261d48c765d0f69
Braulio Zapata <brauliozapata@destiny.ws>
2023-10-25 15:41:52 -0400

    test push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
   
       // Public keys that are exposed to the client
       public: {
-        apiBase: process.env.API_BASE_URL || '/',
+        apiBase: process.env.API_BASE_URL || '/test',
         apiBaseImage: process.env.API_BASE_IMAGE || '/'
       }
     }

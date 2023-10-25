9bd7f387e952858f026d36b5bc13a4c9eb8c9524
Braulio Zapata <brauliozapata@destiny.ws>
2023-10-25 09:56:34 -0400

    change

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
   
       // Public keys that are exposed to the client
       public: {
-        apiBase: process.env.API_BASE_URL || '/localhost',
+        apiBase: process.env.API_BASE_URL || '/localhost:1337',
         apiBaseImage: process.env.API_BASE_IMAGE || '/localhost:1337'
       }
     }

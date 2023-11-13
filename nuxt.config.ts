10b4a1510ba24d505be1c8177cbeba73fbe9cc8a
Braulio Zapata <brauliozapata@destiny.ws>
2023-11-13 09:53:41 -0400

    fix: code

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,7 @@ export default defineNuxtConfig({
       noscript: [
         {
           children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5W3F2VFP"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           tagPosition: 'bodyClose'
         }
       ]

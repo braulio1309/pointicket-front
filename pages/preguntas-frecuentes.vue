735804ef14725117d0fdb67b4e519f17b92914bc
Braulio Zapata <brauliozapata@destiny.ws>
2023-11-17 09:26:43 -0400

    test meta data

--- pages/preguntas-frecuentes.vue
+++ pages/preguntas-frecuentes.vue
@@ -107,6 +107,18 @@
         <ScrollToTop />
     </div>
 </template>
+<script setup lang="ts">
+useHead({
+  title: 'My App',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
 
 <script>
 

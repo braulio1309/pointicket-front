7d1b0a12c686637a6b800406c235a0cc4854763f
Braulio Zapata <brauliozapata@destiny.ws>
2024-09-17 09:20:37 -0400

    translation of menu and add my profile

--- components/header/MobileMenu.vue
+++ components/header/MobileMenu.vue
@@ -10,7 +10,7 @@
                     <NuxtLink :to="link.url" v-html="link.title"></NuxtLink>
                     <span class="submenu-toggle" v-if="link.submenu">
                         <i class="ri-arrow-down-s-line"></i>
-                    </span> 
+                    </span>
                     <ul class="submenu-wrapper" v-if="link.submenu">
                         <li v-for='(link, i) in link.submenu' :key='i'>
                             <NuxtLink :to="link.url" v-html="link.title"></NuxtLink>
@@ -23,44 +23,93 @@
 </template>
 
 <script>
-    import mobileMenuResponsive from '../../common/mobileMenuResponsive';
-    export default {
-        data() {
-            return {
-                menus: [
-                    {
-                        url: '/',
-                        title: 'Inicio',
-                    },
-                    {
-                        url: '/compra-entradas-de-futbol',
-                        title: 'Comprar entradas',
-                    },
-                    {
-                        url: '/vende-entradas-de-futbol',
-                        title: 'Vender entradas',
-                    },
-                    {
-                        url: '/preguntas-frecuentes',
-                        title: 'FAQs',
-                        
-                    },
-                    {
-                        url: '/tour-santiago-bernabeu',
-                        title: 'Tour Santiago Bernabeu',
-                        
-                    },
-                    {
-                        url: '/contacto',
-                        title: 'Contacto',
-                        
-                    },
-                    
-                ]
-            }
-        },
-        mounted() {
-            mobileMenuResponsive();
+import mobileMenuResponsive from '../../common/mobileMenuResponsive';
+export default {
+    data() {
+        return {
+            menus: [
+                {
+                    url: '/',
+                    title: this.$t('Home.home'),
+                },
+                {
+                    url: '/compra-entradas-de-futbol',
+                    title: this.$t('Menu.buy_tickets'),
+                },
+                {
+                    url: '/vende-entradas-de-futbol',
+                    title: this.$t('Menu.sell_tickets'),
+                },
+                {
+                    url: '/preguntas-frecuentes',
+                    title: 'FAQs',
+
+                },
+                {
+                    url: '/tour-santiago-bernabeu',
+                    title: 'Tour Santiago Bernabeu',
+
+                },
+                {
+                    url: '/contacto',
+                    title: this.$t('Menu.contact_us'),
+
+                },
+                {
+                    url: '/profile',
+                    title: this.$t('Menu.profile'),
+
+                },
+
+            ]
         }
+    },
+    watch: {
+        '$i18n.locale'(newLocale, oldLocale) {
+            this.handleLanguageChange(newLocale, oldLocale);
+        }
+    },
+    mounted() {
+        mobileMenuResponsive();
+    },
+    methods: {
+        handleLanguageChange(newLocale, oldLocale) {
+            this.menus = [
+                {
+                    url: '/',
+                    title: this.$t('Home.home'),
+                },
+                {
+                    url: '/compra-entradas-de-futbol',
+                    title: this.$t('Menu.buy_tickets'),
+                },
+                {
+                    url: '/vende-entradas-de-futbol',
+                    title: this.$t('Menu.sell_tickets'),
+                },
+                {
+                    url: '/preguntas-frecuentes',
+                    title: 'FAQs',
+
+                },
+                {
+                    url: '/tour-santiago-bernabeu',
+                    title: 'Tour Santiago Bernabeu',
+
+                },
+                {
+                    url: '/contacto',
+                    title: this.$t('Menu.contact_us'),
+
+                },
+                {
+                    url: '/profile',
+                    title: this.$t('Menu.profile'),
+
+                },
+
+            ]
+        },
     }
+}
 </script>

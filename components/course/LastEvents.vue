74b814810e72f42159c60795bc373f9f1842aa8c
Braulio Zapata <brauliozapata@destiny.ws>
2024-10-17 09:23:25 -0400

    new alert

--- components/course/LastEvents.vue
+++ components/course/LastEvents.vue
@@ -14,9 +14,9 @@
                 <span :style="{ display: event.attributes.description.length == 10 ? 'inline' : 'none', fontSize: '9px', marginTop: '5px', marginBottom: '5px' }">
                     {{ $t('Home.pending_confirmation') }}
                 </span>
-                <span  v-if="event.attributes.count_buy" :style="{ display: event.attributes.count_buy ? 'inline' : 'none', fontSize: '9px', marginTop: '5px', marginBottom: '5px' }">
-                    <img style="margin-left: -20px; margin-top: -10px;" width="50" height="50" src="../../assets/images/compra-entradas/giphy.webp">
-                    Se han vendido {{ Math.floor(Math.random() * (event.attributes.max - event.attributes.min + 1)) + event.attributes.min }} las ultimas 24 horas
+                <span  v-if="event.attributes.count_buy" :style="{ display: event.attributes.count_buy ? 'inline' : 'none', fontSize: '8.5px', marginTop: '5px', marginBottom: '5px' }">
+                    <img style="margin-left: -20px; margin-top: -10px;" width="40" height="40" src="../../assets/images/compra-entradas/giphy.webp">
+                    {{ Math.floor(Math.random() * (event.attributes.max - event.attributes.min + 1)) + event.attributes.min }} entradas vendidas en las ultimas 24 horas
                 </span>
                 <br>
                 <NuxtLink :to="'/tickets/'+type+'r/'+ event.id" class="edu-btn btn-large">{{ (type === 'compra')? $t('Home.buy'): $t('Home.sell') }} <i class="icon-4"></i></NuxtLink>

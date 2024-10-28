caa72bd1538dcb00bc209c14dede1189204fbb01
Braulio Zapata <brauliozapata@destiny.ws>
2024-10-28 11:19:46 -0400

    Add good filter

--- pages/comprar-entradas-de-[type].vue
+++ pages/comprar-entradas-de-[type].vue
@@ -236,6 +236,11 @@ export default {
                 .get(`${query}`)
                 .then((response) => {
                     this.events = response.data.data;
+                    if(this.$route.params.type != 'futbol'){
+                        this.events = this.events.filter(event => event.attributes.type === this.$route.params.type)
+                    }else{
+                        this.events = this.events.filter(event => event.attributes.type === null)
+                    }
                 })
                 .catch((error) => {
                     console.error('Error al buscar al evento', error);
